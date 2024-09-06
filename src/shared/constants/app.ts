d3a344e1ea9de1630a0cd992e3af383ca77b05fd
yoyohoho84 <kirill_1131@mail.ru>
2024-09-06 23:54:22 +0300

    добавил нового админа

--- src/shared/constants/app.ts
+++ src/shared/constants/app.ts
@@ -7,6 +7,6 @@ export const NAME_PROJECT = 'marriage-registration';
 export const APP_AVATAR = '';
 export const ROUTE_URL =
   process.env.NODE_ENV === 'production' ? '/marriage-registration' : '';
-export const ADMINS = [738505078, 769019442];
+export const ADMINS = [738505078, 769019442, 859733533];
 export const SHARING_TEXT = `👉 Наше приложение - vk.me/public216163041 Пиши "Начать"`;
 export const URL_PROXY = 'https://proxy.rzgn.ru';
